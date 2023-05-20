0ad4e217ee1892e8a6f7bcce1bf97f5213d0508c
User <povad1rso2progg@gmail.com>
2023-05-20 11:08:08 +0300

    classwork

--- semester2/lesson6/main.py
+++ semester2/lesson6/main.py
@@ -1,12 +1,13 @@
-from aiogram import Dispatcher, Bot, executor, types
 import logging
+
+from aiogram import Dispatcher, Bot, executor, types
 from aiogram.dispatcher import FSMContext
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 import os 
 from dotenv import load_dotenv
 from aiogram.dispatcher.filters.builtin import CommandStart
 import requests
-
+from parse_weather import parse_weather
 load_dotenv()
 
 TOKEN = os.getenv('TOKEN')
@@ -20,10 +21,10 @@ dp = Dispatcher(bot, storage=MemoryStorage())
 async def start(message: types.Message):
     await message.answer(text='Hello I am weather bot!')    
 
-@dp.message_handler(text='Hello')
-async def greetings(message:types.Message):
-    name = message.from_user.full_name
-    await message.answer(f'Hello {name}.\nMy name is Geralt')
+# @dp.message_handler(text='Hello')
+# async def greetings(message:types.Message):
+#     name = message.from_user.full_name
+#     await message.answer(f'Hello {name}.\nMy name is Geralt')
 
 @dp.message_handler(commands=['Weather'])
 async def weather(message:types.Message):
@@ -33,23 +34,18 @@ async def weather(message:types.Message):
 @dp.message_handler()
 async def handle_city(message: types.Message):
     city = message.text
-    weather_data = await parse_weather(city)
+    weather_data = parse_weather(city)
     weather = weather_data['main']
     precipitation = weather_data['weather'][0]['main'] 
     humidity = weather['humidity']
+    pressure = weather['pressure'] *  0.0075
     temp = weather['temp'] - 273.15
     await message.answer(f"Weather in {city}:"
                         f"\nTemperature: {round(temp)}°C"
                         f"\nHumidity: {humidity}%"
-                        f"\nSky: {precipitation}")
-    
-async def parse_weather(city: str):
-    api_key = '0ebe025b8d93f180657fa5a4ccf33ba7'
-    url = f'https://api.openweathermap.org/data/2.5/weather?&appid={api_key}&q={city}'
-    response = requests.get(url)
+                        f"\nSky: {precipitation}"
+                        f"\nPressure: {round(pressure, 2)}")
 
-    return response.json()
-    
 
 @dp.message_handler(commands=['info'])
 async def user_info(message: types.Message):
